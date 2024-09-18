ada7e311840a31667f13a51c0d071d5333ddaf2d
Joao Victor Carvalho Machado <machado.joaovictor@outlook.com>
2024-09-18 13:01:55 -0300

    :sparkles: Entities

---
+++ src/main/java/com/github/joaovictorcmd/customerscrud/CustomersCrudApplication.java
@@ -0,0 +1,13 @@
+package com.github.joaovictorcmd.customerscrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersCrudApplication.class, args);
+	}
+
+}
